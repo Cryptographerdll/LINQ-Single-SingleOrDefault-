53b84e73f30670afec354c7db7ce223e64fbff4d
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-16 19:08:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+
+namespace LINQ_Single___SingleOrDefault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Names = { "IronMan" };
+
+            var FirstItem = Names.Single(); // IronMan
+
+            #region comment 
+            // In Single(); method we get the item exist in string list but the list must contain one item .
+            // In case we Give unexistant word or We declar an empty string, Programm will Give Error console soo we must use the SingleOrDefault(); Method .
+            #endregion
+
+            Console.Write(FirstItem); // Does not exist soo we ask the programm if not exist == Null return True . 
+        }
+    }
+}
